c8405e8f1b7c935f570cfa54bf1c031cc7c82ea2
akiet00 <akiet00@yahoo.com>
2017-12-28 03:24:00 -0600

    update

--- dataProcess.js
+++ dataProcess.js
@@ -1,37 +1,43 @@
 $.ajax({
-  url:"resources/specs.csv",
+  url: "https://raw.githubusercontent.com/akiet00/sell/master/resources/specs.csv",
   datatype: 'text',
 }).done(parseData);
 
-function parseData(data){
+function parseData(data) {
   var allRows = data.split(/\r?\n|\r/);
   var table = "<table>"
   //Loop through each row of the table
-  for(var i=0; i<allRows.length; i++){
-    if(i === 0){  //Head row
+  for (var i = 0; i < allRows.length; i++) {
+    if (i === 0) { //First row
       table += "<thead>";
       table += "<tr>";
-    } else{
+    } else {
       table += "<tr>";
     }
     //Loop through each cell in a row
     var rowCells = allRows[i].split(',');
-    for(var j=0; j<rowCells.length; j++){
-      if(i === 0){
+    for (var j = 0; j < rowCells.length; j++) {
+      if (i === 0) { //First row
         table += "<th>";
         table += rowCells[j];
         table += "</th>";
-      }else{
-        table += "<td>";
-        table += rowCells[j];
-        table += "</td>";
+      } else { //Not first row
+        if (j === 3) { //column with hyperlink
+          table += "<td>";
+          table += "<a href=\'" + rowCells[j] + "\' target='_blank'>Buy</a>";
+          table += "</td>";
+        } else { //column without hyperlink
+          table += "<td>";
+          table += rowCells[j];
+          table += "</td>";
+        }
       }
     }
-    if(i === 0){ //End of the first row
+    if (i === 0) { //End of the first row
       table += "</tr>";
       table += "</thead>";
       table += "<tbody>"
-    }else{
+    } else {
       table += "</tr>";
     }
   }
@@ -39,5 +45,5 @@ function parseData(data){
   table += '</tbody>';
   table += '</table>';
   //Append to spec sheet
-  $('.table-container').append(table);
+  $('#table-container').append(table);
 }
